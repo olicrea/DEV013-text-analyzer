2a70276558bfa2b5ca5fd3bec290a8901ab14427
olicrea <olimar.digital@gmail.com>
2023-12-12 21:46:42 -0300

    inicio con selectores del DOM y eventos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//Uso de selectores del DOM
+
+const textarea = document.querySelector("textarea") //querySelector seleccionará el primer elemento que coincida con la etiqueta textarea. Si hubiese más de un elemento que coincida lo mejor sería usar identificador único 
+
+const button = document.getElementById("reset-button");
+
+//Manejo de eventos del DOM
+
+button.addEventListener("click", resetTextarea);
+textarea.addEventListener("keyup", appAnalyzer);
+
+function resetTextarea() {
+  textarea.value = "";
+  textarea.placeholder = "Write what you want to analyze...";
+}
+
+function appAnalyzer() {
+  //
+}
