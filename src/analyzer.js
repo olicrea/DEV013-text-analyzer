3d5c10663abd7361c91ab56a37f55eea53b5a26f
olicrea <olimar.digital@gmail.com>
2023-12-16 13:18:33 -0300

    5 funciones listas del objeto analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -5,7 +5,7 @@ const analyzer = {
     //- SEUDO-CÓDIGO:
     //- Separar las palabras a través de algún método
     //- Tomar en cuenta las palabras con letras sin excluir las que contengan números de alguna forma
-    const words = text.split(" ");
+    const words = text.trim().split(' ');
     //- Retornar el conteo de palabras
     return words.length;
   },
@@ -29,7 +29,7 @@ const analyzer = {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
     //- SEUDO CÓDIGO:
     //- obtener cada caracter excluyendo espacios y signos de puntuación
-    const letters = text.trim().replace(/[^a-zA-Z\d]/g, '');
+    const letters = text.replace(/[^a-zA-Z\d]/g, '');
     // console.log(letters);
     return letters.length;
   },
@@ -39,17 +39,35 @@ const analyzer = {
   //______________________________
 
 
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //- SEUDO CÓDIGO:
+    //- saber cuántas palabras hay separándolas a través de los espacios entre ellas y alrededor de ellas
+    const words = text.trim().split(' ');
+    //- saber la cantidad de palabras en el array
+    const totalWords = words.length;
+    //- sumar los largos de las palabras uno por uno a través de iteración
+    let totalSum = 0;
+    for (let i = 0; i < totalWords; i++) {
+      totalSum += words[i].length;
+    }
+    //- calcular promedio
+    const averageWordLength = totalSum / totalWords;
+    //- retornar el promedio, con 2 decimales
+    return Number(averageWordLength.toFixed(2));
+  },
+  //______________________________
   
+  
+  getNumberCount: (text) => {
+    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = text.replace(/[^0-9]/g, '');
+    return numbers.length;
   },
   //______________________________
 
 
-  /*getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
-  },
-  getNumberSum: (text) => {
+  /*getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },*/
 };
@@ -57,5 +75,8 @@ console.log(analyzer.getWordCount('Felicidad y paz 123 oli89 para234guas interio
 console.log(analyzer.getCharacterCount('abc def hij 123 456'));//19
 console.log(analyzer.getCharacterCountExcludingSpaces(' Felicidad 123 ;- oli345 y paz '));//22
 console.log(analyzer.getCharacterCountExcludingSpaces(' Felicidad 123 ;- y paz '));//16
+console.log(analyzer.getAverageWordLength(' luci mar oscar '));//4
+console.log(analyzer.getNumberCount(' luci122 6 7 89 ma7ri oscar '));//8
+
 
 export default analyzer;
